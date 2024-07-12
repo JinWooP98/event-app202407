16e9d3d97f35455e61a5a5c2f0964bf2c1d742e7
JinWooP98 <qwdk0406@naver.com>
2024-07-12 11:30:27 +0900

    # password 입력 처리

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './SignUpForm.module.scss';
 import EmailInput from './EmailInput';
-import VerificationInput from "./VerificationInput";
-import ProgressBar from "../ProgressBar";
+import VerificationInput from './VerificationInput';
+import ProgressBar from '../ProgressBar';
+import PasswordInput from './PasswordInput';
 
 const SignUpForm = () => {
 
@@ -12,37 +13,71 @@ const SignUpForm = () => {
     // 단계가 성공적으로 완료되었는지
     const [success, setSuccess] = useState(false);
 
-    // 이메일 중복확인이 끝났을 때 호출될 함수
-    const emailSuccessHandler = () => {
+    // 입력된 이메일
+    const [enteredEmail, setEnteredEmail] = useState('');
+    // 입력된 패스워드
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const [passwordIsVaild, setPasswordIsValid] = useState(false);
 
+    // 회원가입 버튼 활성화 여부
+    const [activeButton, setActiveButton] = useState(false);
+
+    // 다음 단계로 넘어가는 함수
+    const nextStep = () => {
         setSuccess(true);
 
         setTimeout(() => {
-            setStep(2);
+            setStep(prevStep => prevStep + 1);
             setSuccess(false);
         }, 1500);
-
     };
 
+    // 이메일 중복확인이 끝났을 때 호출될 함수
+    const emailSuccessHandler = (email) => {
+        setEnteredEmail(email);
+        nextStep();
+    };
 
+    const passwordSuccessHandler = (password, isValid) => {
+        setEnteredPassword(password);
+        setPasswordIsValid(isValid);
+    };
 
+    useEffect(() => {
+        // 활성화 여부 감시
+        const isActive = enteredEmail && passwordIsVaild;
+        setActiveButton(isActive);
+
+    }, [enteredEmail, passwordIsVaild]);
 
     return (
-    <div className={styles.signupForm}>
-      <div className={styles.formStepActive}>
+        <div className={styles.signupForm}>
+            <div className={styles.formStepActive}>
 
-          {/*{step === 1 && <EmailInput onSuccess={emailSuccessHandler} />}*/}
+                {step === 1 && <EmailInput onSuccess={emailSuccessHandler} />}
 
-          {/*{step === 2 && <VerificationInput/>}*/}
+                {step === 2 &&
+                    <VerificationInput
+                        email={enteredEmail}
+                        onSuccess={() => nextStep()}
+                    />
+                }
 
+                {step === 3 && <PasswordInput onSuccess={passwordSuccessHandler} />}
 
-          <VerificationInput />
+                {activeButton &&
+                    <div>
+                        <button>
+                            회원가입 완료
+                        </button>
+                    </div>
+                }
 
-          {success && <ProgressBar/>}
+                {success && <ProgressBar />}
 
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 
 export default SignUpForm;
--- src/components/auth/VerificationInput.js
+++ src/components/auth/VerificationInput.js
@@ -1,14 +1,21 @@
 import React, {useEffect, useRef, useState} from 'react';
 import styles from './SignUpForm.module.scss'
-import {logDOM} from "@testing-library/react";
+import {debounce} from "lodash/function";
+import {AUTH_URL} from "../../config/host-config";
 
-const VerificationInput = () => {
+const VerificationInput = ({email, onSuccess}) => {
 
     // 여러개의 컴포넌트에 ref를 거는 방법
     const inputsRef = useRef([]);
 
     // 입력한 인증코드를 저장
-    const [codes, setCodes] = useState([]);
+    const [codes, setCodes] = useState(Array(4).fill(''));
+
+    // 에러메시지 저장
+    const [error, setError] = useState('');
+
+    // 타이머 시간
+    const [timer, setTimer] = useState(300);
 
     // 다음 칸으로 포커스를 이동하는 함수
     const focusNextInput = (index) => {
@@ -20,12 +27,30 @@ const VerificationInput = () => {
     };
 
     //서버에 검증요청 보내기
-    const verifyCode = async (code) => {
-        console.log('요청 전송!', code);
-    };
+    const verifyCode = debounce (async (code) => {
+
+        const response = await fetch(`${AUTH_URL}/code?email=${email}&code=${code}`);
+        const flag = await  response.json();
+
+        // 검증에 실패했을때
+        if(!flag) {
+            setError('유효하지 않거나 만료된 코드입니다. 인증코드를 재발송합니다.');
+            // 인풋창 비우기
+            setCodes([...Array(4).fill('')]);
+            // 타이머 리셋
+            setTimer(300);
+
+            return;
+        }
+
+        // 검증 성공시
+        onSuccess();
+
+    }, 1500);
 
     const changeHandler = (index, inputValue) => {
-        const updatedCodes = [...codes, inputValue];
+        const updatedCodes = [...codes];
+        updatedCodes[index -1] = inputValue;
         console.log(updatedCodes);
         // codes 변수에 입력한 숫자 담아놓기
         setCodes(updatedCodes);
@@ -50,6 +75,17 @@ const VerificationInput = () => {
     useEffect(() => {
         // 처음엔 첫번째 칸에 포커싱
         inputsRef.current[0].focus();
+
+        // 타이버 설정
+        const intervalId = setInterval(() => {
+            setTimer(prevTime => prevTime > 0 ? prevTime - 1 : 0);
+        }, 1000);
+
+        // 타이머 리셋
+        return () => {
+            clearInterval(intervalId);
+        };
+
     }, []);
 
     return (
@@ -58,7 +94,6 @@ const VerificationInput = () => {
                 Step 2: 이메일로 전송된 인증번호 4자리를 입력해주세요.
             </p>
             <div className={styles.codeInputContainer}>
-
                 {
                     Array.from(new Array(4)).map((_, index) => (
                         <input
@@ -68,12 +103,13 @@ const VerificationInput = () => {
                             className={styles.codeInput}
                             maxLength={1}
                             onChange={(e) => changeHandler(index + 1, e.target.value)}
+                            value={codes[index]}
                         />
                     ))
                 }
-
             </div>
-
+            <div className={styles.timer}>{`${'0' + Math.floor(timer / 60)}:${('0' + (timer % 60)).slice(-2)}`}</div>
+            {error && <p className={styles.errorMessage}>{error}</p>}
         </>
     );
 };
